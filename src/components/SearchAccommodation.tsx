f013985126fe988359f4a1742f591360ca4d1ee0
vulinana <sm.vulinana@gmail.com>
2023-05-02 22:40:10 +0200

    small change in search component

--- src/components/SearchAccommodation.tsx
+++ src/components/SearchAccommodation.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, Flex, Input} from '@chakra-ui/react'
+import {Box, Button, Flex, FormControl, FormLabel, Input} from '@chakra-ui/react'
 import { useEffect, useState } from 'react';
 import { Form } from 'react-router-dom';
 import { useApplicationStore } from '../store/application.store';
@@ -48,14 +48,26 @@ export const SearchAccommodation = ({sendData}: Props) => {
 
     return (
         <>
-        <Box padding="10">
+        <Box padding="2">
         <Form onSubmit={onSubmit}>
             <Flex flexDirection='row'>
-            <Input name="city" onChange={handleCityInputChange} placeholder='City'></Input>
-            <Input name="guests" onChange={handleGuestsInputChange} placeholder='Guests' type='number' min='1'></Input>
-            <Input name="startDate" onChange={handleStartDateInputChange} placeholder='Start date' type='date'></Input>
-            <Input name="endDate"  onChange={handleEndDateInputChange} placeholder='End date' type='date'></Input>
-            <Button type="submit" mb={'15px'} width="200px">Search</Button>
+            <FormControl>
+                <FormLabel mb='0'>City</FormLabel>
+                <Input name="city" onChange={handleCityInputChange} placeholder='Enter city'></Input>
+            </FormControl>
+            <FormControl>
+                <FormLabel mb='0'>Guests</FormLabel>
+                <Input name="guests" onChange={handleGuestsInputChange} placeholder='Enter number of guests' type='number' min='1'></Input>
+            </FormControl>
+            <FormControl>
+                <FormLabel mb='0'>Start Date</FormLabel>
+                <Input name="startDate" onChange={handleStartDateInputChange} type='date'></Input>
+            </FormControl>
+            <FormControl>
+                <FormLabel mb='0'>End Date</FormLabel>
+                <Input name="endDate"  onChange={handleEndDateInputChange} type='date'></Input>
+            </FormControl>
+            <Button type="submit" mt='6' mb={'15px'} width="200px">Search</Button>
             </Flex>
         </Form>
         </Box>
