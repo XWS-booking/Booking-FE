6e602db8a13e82edcdb78e34e9f8659ba82c51b3
vulinana <sm.vulinana@gmail.com>
2023-06-08 12:51:21 +0200

    open websocket connection and display message to the appropriate recipient

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,37 @@ import { Box, Button, Flex, Img, Text } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+import { useApplicationStore } from './store/application.store';
 
 function App() {
+
+  const user = useApplicationStore((state) => state.user)
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8080/ws'); 
+
+    socket.addEventListener('open', () => {
+      console.log('WebSocket connection established.');
+    });
+
+    socket.addEventListener('message', (event) => {
+      const notification = JSON.parse(event.data);
+      console.log('Received notification:', notification);
+
+      if (user?.id == notification.recipient) 
+        toast.info(notification.message);
+    });
+
+    return () => {
+      socket.close();
+      console.log("closed socket")
+    };
+  }, []);
+
+
   return (
     <Flex w='100%' h='100%' minH='100vh' direction='column'>
       <Header></Header>
